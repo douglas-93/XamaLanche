ef04a6a293248277cd282260116543086a447cf6
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-07-30 21:00:53 -0300

    Start

---
+++ XamaLanche/src/main/java/com/dmm/xamalanche/XamaLancheApplication.java
@@ -0,0 +1,13 @@
+package com.dmm.xamalanche;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XamaLancheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XamaLancheApplication.class, args);
+    }
+
+}
---
+++ XamaLanche/src/test/java/com/dmm/xamalanche/XamaLancheApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmm.xamalanche;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XamaLancheApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

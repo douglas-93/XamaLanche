fa7e04168e2012d81968a97c1976b4ba86009a3c
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-08-01 21:36:20 -0300

    Implementação crud service e resource

--- XamaLanche/src/main/java/com/dmm/xamalanche/resource/LojaResource.java
+++ XamaLanche/src/main/java/com/dmm/xamalanche/resource/LojaResource.java
@@ -3,21 +3,49 @@ package com.dmm.xamalanche.resource;
 import com.dmm.xamalanche.model.Loja;
 import com.dmm.xamalanche.service.LojaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/loja")
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RequestMapping(value = "/loja")
 public class LojaResource {
 
     @Autowired private LojaService lojaService;
 
-    @GetMapping()
-    public List<Loja> findAll(){
-        return lojaService.findAll();
+    @GetMapping
+    public ResponseEntity<List<Loja>> findAll(){
+        List<Loja> lojas = lojaService.findAll();
+        return ResponseEntity.status(HttpStatus.OK).body(lojas);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Loja> findById(@PathVariable Integer id) {
+        Loja loja = lojaService.findById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(loja);
+    }
+
+    @PostMapping
+    public ResponseEntity<Loja> create(@RequestBody Loja novaLoja) {
+        Loja loja = lojaService.create(novaLoja);
+        return ResponseEntity.status(HttpStatus.CREATED).body(loja);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Loja> update(@PathVariable Integer id, @RequestBody Loja lojaAtualizada) {
+        Loja loja = lojaService.update(lojaAtualizada, id);
+        return ResponseEntity.status(HttpStatus.OK).body(loja);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Integer id) {
+        lojaService.delete(id);
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    }
+
+
 }
--- XamaLanche/src/main/java/com/dmm/xamalanche/service/LojaService.java
+++ XamaLanche/src/main/java/com/dmm/xamalanche/service/LojaService.java
@@ -3,18 +3,48 @@ package com.dmm.xamalanche.service;
 import com.dmm.xamalanche.model.Loja;
 import com.dmm.xamalanche.repository.LojaRepository;
 import com.dmm.xamalanche.utils.service.BaseCrudService;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
-public class LojaService implements BaseCrudService<Loja,Integer> {
+public class LojaService implements BaseCrudService<Loja, Integer> {
 
-    @Autowired private LojaRepository repository;
+    @Autowired
+    private LojaRepository repository;
 
     public List<Loja> findAll() {
         return repository.findAll();
+    }
+
+    public Loja findById(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new EntityNotFoundException("Loja não encontrada!"));
+    }
+
+    public Loja create(Loja loja) {
+        return repository.save(loja);
+    }
+
+    public void delete(Integer id) {
+        Loja loja = this.findById(id);
+        repository.delete(loja);
+    }
+
+    public Loja update(Loja lojaAtualizada, Integer id) {
+        Loja loja = this.findById(id);
+        updateEntity(lojaAtualizada, loja);
+        return repository.save(loja);
+    }
+
+    public void updateEntity(Loja lojaAtualizada, Loja loja) {
+        loja.setNome(lojaAtualizada.getNome());
+        loja.setCardapio(lojaAtualizada.getCardapio());
+        loja.setEndereco(lojaAtualizada.getEndereco());
+        loja.setPedidoMinimo(loja.getPedidoMinimo());
+        loja.setVersao(loja.getVersao());
     }
 
 }

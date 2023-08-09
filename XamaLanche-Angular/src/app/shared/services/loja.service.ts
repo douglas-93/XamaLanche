cfaef698fc6c65009a60290bc97c147e067bda8a
Marcos Leao <marcosplaignier@hotmail.com>
2023-08-08 21:23:02 -0300

    Extendendo o AbstractCrudService

--- XamaLanche-Angular/src/app/shared/services/loja.service.ts
+++ XamaLanche-Angular/src/app/shared/services/loja.service.ts
@@ -1,10 +1,10 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {Loja} from "../models/loja";
-import {AbstractCRUDService} from "./abstractCRUD.service";
+import {AbstractCrudService} from "./abstract.crud.service";
 
 @Injectable()
-export class LojaService extends AbstractCRUDService<Loja>{
+export class LojaService extends AbstractCrudService<Loja>{
 
   constructor(http: HttpClient) {
     super(http, "loja");

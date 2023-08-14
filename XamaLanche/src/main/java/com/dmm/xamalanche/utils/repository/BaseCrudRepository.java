e89a318ba7230790fd4fa31dabbc8603499697b1
Marcos Leao <marcosplaignier@hotmail.com>
2023-08-13 22:01:12 -0300

    Implementando BaseCrudService e AbstractCrudService que serao as bases de todos os Services ja contendo metodos padroes

--- XamaLanche/src/main/java/com/dmm/xamalanche/utils/repository/BaseCrudRepository.java
+++ XamaLanche/src/main/java/com/dmm/xamalanche/utils/repository/BaseCrudRepository.java
@@ -7,13 +7,4 @@ import java.util.List;
 
 public interface BaseCrudRepository<T,ID> extends JpaRepository<T,ID> {
 
-//    T saveOrUpdate(T entity);
-//
-//    T update(T entity);
-//
-//    T getById(ID id);
-//
-//    <X> List<X> getRelationship(Integer id, String propertyPath);
-
-
 }
---
+++ XamaLanche/src/main/java/com/dmm/xamalanche/utils/service/AbstractCrudService.java
@@ -0,0 +1,104 @@
+package com.dmm.xamalanche.utils.service;
+
+import com.dmm.xamalanche.utils.exceptions.AbstractServiceException;
+import com.dmm.xamalanche.utils.repository.BaseCrudRepository;
+import org.hibernate.service.spi.ServiceException;
+
+import java.util.List;
+
+public abstract class AbstractCrudService<T,ID> implements BaseCrudService<T, ID> {
+
+    //Metodo padrao de Save
+    //Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo save do repositorio padrao
+    public T save(T entity) throws AbstractServiceException {
+
+        Object var2;
+        try {
+            this.validate(entity);
+            this.beforeSave(entity);
+            entity = (T) this.getRepository().save(entity);
+            this.afterSave(entity);
+            var2 = entity;
+        } finally {
+        }
+
+        return (T) var2;
+    }
+
+    //Metodo padrao de Update
+    //Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo update do repositorio padrao
+    public T update(T entity) throws ServiceException {
+
+        Object var2;
+        try {
+            this.validate(entity);
+            this.beforeUpdate(entity);
+            entity = (T) this.getRepository().save(entity);
+            this.afterUpdate(entity);
+            var2 = entity;
+        } finally {
+        }
+
+        return (T) var2;
+    }
+
+    //Metodo padrao de Delete
+    //Nele ha possibilidade de validar ou executar algo antes ou apos a execucao do metodo delete do repositorio padrao
+    public T delete(T entity) throws ServiceException {
+
+        Object var2;
+        try {
+            this.validateDelete(entity);
+            this.beforeDelete(entity);
+            this.getRepository().delete(entity);
+            this.afterDelete(entity);
+            var2 = entity;
+        } finally {
+
+        }
+
+        return (T) var2;
+    }
+
+    //Metodo padrao de buscar todos os dados da entity
+    public List<T> findAll() {
+        return this.getRepository().findAll();
+    }
+
+    //Metodo padrao de buscar por ID da entity
+
+    public T getById(ID id) {
+        return (T) this.getRepository().getById(id);
+    }
+
+
+    protected void validate(T entity) throws ServiceException {
+    }
+
+    protected void validateDelete(T entity) throws ServiceException {
+    }
+
+    protected void beforeSave(T entity) throws ServiceException {
+    }
+
+    protected void afterSave(T entity) throws ServiceException {
+    }
+
+    protected void beforeUpdate(T entity) throws ServiceException {
+    }
+
+    protected void afterUpdate(T entity) throws ServiceException {
+    }
+
+    protected void beforeDelete(T entity) throws ServiceException {
+    }
+
+    protected void afterDelete(T entity) throws ServiceException {
+    }
+
+
+    @Override
+    public BaseCrudRepository getRepository() {
+        return null;
+    }
+}

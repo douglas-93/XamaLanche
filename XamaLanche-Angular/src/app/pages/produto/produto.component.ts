1a8ac99102911e8d1806c4329f34005c03145780
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-08-20 14:23:47 -0300

    Inicio do cadastro de produto

--- XamaLanche-Angular/src/app/pages/produto/produto.component.ts
+++ XamaLanche-Angular/src/app/pages/produto/produto.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ModeEnum} from "../../shared/enum/mode.enum";
+import {Router} from "@angular/router";
+import {Produto} from "../../shared/models/Produto";
 
 @Component({
-  selector: 'app-produto',
-  templateUrl: './produto.component.html',
-  styleUrls: ['./produto.component.scss']
+    selector: 'app-produto',
+    templateUrl: './produto.component.html',
+    styleUrls: ['./produto.component.scss']
 })
 export class ProdutoComponent implements OnInit {
+    mode: ModeEnum = ModeEnum.List;
+    isEdit: boolean = false;
+    produto: Produto;
+    produtos: Produto[];
+    dxSelectBoxAtivoOptions: any = ['Ativo', 'Inativo'];
 
-  constructor() { }
+    constructor(private router: Router) {
+        this.editCheck();
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+        this.produto = new Produto();
+    }
 
+    novoProduto(): void {
+        this.router.navigate(['pages', 'produto', 'edit'])
+    }
+
+    private editCheck() {
+        this.isEdit = this.router.url.split('/').includes('edit');
+        this.mode = this.isEdit ? ModeEnum.Edit : ModeEnum.List;
+    }
+
+    protected readonly ModeEnum = ModeEnum;
 }

1a25058f94028e69909cf9d9455e2179cd34650c
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-08-20 14:23:19 -0300

    retirado o exemplo comentado e adicionado as outras paginas

--- XamaLanche-Angular/src/app/app-navigation.ts
+++ XamaLanche-Angular/src/app/app-navigation.ts
@@ -1,41 +1,37 @@
 export const navigation = [
-  {
-    text: 'Home',
-    path: '/home',
-    icon: 'home'
-  },
-/*  {
-    text: 'Examples',
-    icon: 'folder',
-    items: [
-      {
-        text: 'Profile',
-        path: '/profile'
-      },
-      {
-        text: 'Tasks',
-        path: '/tasks'
-      }
-    ]
-  },*/
-  {
-    text: 'Loja',
-    path: '/pages/loja',
-    icon: 'card'
-  },
-  {
-    text: 'Produto',
-    path: '/pages/produto',
-    icon: 'food'
-  },
-  {
-    text: 'Cardapio',
-    path: '/pages/cardapio',
-    icon: 'product'
-  },
-  {
-    text: 'Cliente',
-    path: '/pages/cliente',
-    icon: 'user'
-  },
+    {
+        text: 'Home',
+        path: '/home',
+        icon: 'home'
+    },
+    {
+        text: 'Loja',
+        path: '/pages/loja',
+        icon: 'card'
+    },
+    {
+        text: 'Produto',
+        path: '/pages/produto',
+        icon: 'food'
+    },
+    {
+        text: 'Cardapio',
+        path: '/pages/cardapio',
+        icon: 'product'
+    },
+    {
+        text: 'Acrescimo',
+        path: '/pages/acrescimo',
+        icon: 'fa fa-cutlery'
+    },
+    {
+        text: 'Promocao',
+        path: '/pages/promocao',
+        icon: 'money'
+    },
+    {
+        text: 'Cliente',
+        path: '/pages/cliente',
+        icon: 'user'
+    },
 ];

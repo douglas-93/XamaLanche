faeeea9702a02a8f5b672d5da9a0db14d203ae72
Marcos Leao <marcosplaignier@hotmail.com>
2023-09-01 22:35:56 -0300

    Acertos para novo modelo de abstração

--- XamaLanche-Angular/src/app/pages/cliente/cliente.component.ts
+++ XamaLanche-Angular/src/app/pages/cliente/cliente.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, Injector, ViewChild} from '@angular/core';
 import {Cliente} from '../../shared/models/cliente';
 import {ClienteService} from '../../shared/services/cliente.service';
 import {Router} from '@angular/router';
@@ -9,97 +9,128 @@ import {FormatAndMaks} from '../../shared/utils/FormatAndMaks';
 import {DxFormComponent} from 'devextreme-angular';
 import {EnderecoFormComponent} from '../../shared/components/endereco-form/endereco-form.component';
 import {CrudComponent} from '../../shared/components/base-crud/crud.component';
+import {AbstractCrud} from "../../shared/crud/abstract.crud";
+import {Observable} from "rxjs";
 
 
 @Component({
-  selector: 'app-cliente',
-  templateUrl: './cliente.component.html',
-  styleUrls: ['./cliente.component.scss']
+    selector: 'app-cliente',
+    templateUrl: './cliente.component.html',
+    styleUrls: ['./cliente.component.scss']
 })
-export class ClienteComponent {
+export class ClienteComponent extends AbstractCrud<Cliente, any>{
 
-  @ViewChild('formularioCliente', {static: false})
-  formularioCliente: DxFormComponent;
+    @ViewChild('formularioCliente', {static: false})
+    formularioCliente: DxFormComponent;
 
-  @ViewChild('formularioEndereco', {static: false})
-  formularioEndereco: EnderecoFormComponent;
+    @ViewChild('formularioEndereco', {static: false})
+    formularioEndereco: EnderecoFormComponent;
 
-  @ViewChild('crud', {static: false}) crud: CrudComponent;
+    @ViewChild('crud', {static: false}) crud: CrudComponent;
 
-  mode: ModeEnum = ModeEnum.List;
-  loadingVisible: boolean = false;
-  clientes: Cliente[];
-  cliente: Cliente;
-  protected readonly ModeEnum = ModeEnum;
+    mode: ModeEnum = ModeEnum.List;
+    loadingVisible: boolean = false;
+    clientes: Cliente[];
+    cliente: Cliente;
+    protected readonly ModeEnum = ModeEnum;
 
-  constructor(private clienteService: ClienteService,
-              private router: Router) {
-    this.cliente = new Cliente();
-    /*  if (this.crud.edit) {
-        this.mode = ModeEnum.Edit
-      }*/
-  }
 
-  ngOnInit() {
-    let isEditing: boolean = this.router.url.split('/').includes('edit');
-    if (isEditing) {
-      this.mode = ModeEnum.Edit;
+    constructor(injector: Injector,
+                private mainService:ClienteService,
+                private router:Router) {
+        super(injector, "/clientes");
     }
-  }
 
-
-  buscaClientes() {
-    this.loadingVisible = true;
-    this.clienteService.getAll().subscribe(res => {
-        if (res.ok) {
-          this.clientes = res.body!;
-          this.loadingVisible = false;
+    protected override doOnInit() {
+        let isEditing: boolean = this.router.url.split('/').includes('edit');
+        if (isEditing) {
+            this.mode = ModeEnum.Edit;
+            this.model = new Cliente();
         }
-      },
-      error => {
-        this.loadingVisible = false;
-        notify('Não foi possível buscar os clientes cadastrados', 'error', 3000);
-      }
-    );
-  }
+    }
 
-  novoCliente() {
-    this.router.navigate(['pages', 'cliente', 'edit']);
-  }
-
-  salvarCliente() {
-    if (this.formularioEndereco.getGridData().length > 0 && this.formularioCliente.instance.validate().isValid) {
-      this.cliente.endereco = this.formularioEndereco.getGridData();
-      this.clienteService.save(this.cliente).subscribe(res => {
-          if (res.ok) {
-            notify('Cliente salvo com sucesso', 'success', 3000);
-            this.crud.voltar();
-            return;
-          }
-          if (res.status != 201) {
-            notify('Algo deu errado ao tentar salvar', 'error', 3000);
-            return;
-          }
-        },
-        error => {
-          notify('Algo deu errado ao tentar salvar', 'error', 3000);
-          return;
+    override validateSave() {
+        if(this.model.nome == null){
+            notify('Por favor, preencha o Nome do cliente', 'error', 3000);
+            return false;
         }
-      );
-    }
-    if (!this.formularioCliente.instance.validate().isValid) {
-      notify('Por favor, preencha os dados do cliente', 'error', 3000);
-      return;
-    }
-    if (this.formularioEndereco.getGridData().length === 0) {
-      notify('Por favor, adicione ao menos um endereço antes de prosseguir', 'error', 3000);
-      return;
+        if(this.model.telefone == null){
+            notify('Por favor, preencha o Telefone do cliente', 'error', 3000);
+            return false;
+        }
+        if(this.model.endereco == null || this.model.endereco?.length <= 0 ){
+            notify('Por favor, preencha pelo menos um endereço do cliente', 'error', 3000);
+            return false;
+        }
+
+        return true;
+        super.validateSave();
     }
 
-  }
+    override beforeDoSave(): Observable<any> | null {
+          if (this.formularioEndereco.getGridData().length > 0 && this.formularioCliente.instance.validate().isValid) {
+              this.model.endereco = this.formularioEndereco.getGridData();
+          }
+        return super.beforeDoSave();
+    }
 
-  formatPhone(e: ChangeEvent) {
-    let phone = e.component.instance().option('value');
-    this.cliente.telefone = FormatAndMaks.formatPhoneNumber(phone!);
-  }
+
+    // buscaClientes() {
+    //   this.loadingVisible = true;
+    //   this.clienteService.getAll().subscribe(res => {
+    //       if (res.ok) {
+    //         this.clientes = res.body!;
+    //         this.loadingVisible = false;
+    //       }
+    //     },
+    //     error => {
+    //       this.loadingVisible = false;
+    //       notify('Não foi possível buscar os clientes cadastrados', 'error', 3000);
+    //     }
+    //   );
+    // }
+    //
+    novoCliente() {
+        this.router.navigate(['pages', 'cliente', 'edit']);
+    }
+    //
+    // salvarCliente() {
+    //   if (this.formularioEndereco.getGridData().length > 0 && this.formularioCliente.instance.validate().isValid) {
+    //     this.cliente.endereco = this.formularioEndereco.getGridData();
+    //     this.clienteService.save(this.cliente).subscribe(res => {
+    //         if (res.ok) {
+    //           notify('Cliente salvo com sucesso', 'success', 3000);
+    //           this.crud.voltar();
+    //           return;
+    //         }
+    //         if (res.status != 201) {
+    //           notify('Algo deu errado ao tentar salvar', 'error', 3000);
+    //           return;
+    //         }
+    //       },
+    //       error => {
+    //         notify('Algo deu errado ao tentar salvar', 'error', 3000);
+    //         return;
+    //       }
+    //     );
+    //   }
+    //   if (!this.formularioCliente.instance.validate().isValid) {
+    //     notify('Por favor, preencha os dados do cliente', 'error', 3000);
+    //     return;
+    //   }
+    //   if (this.formularioEndereco.getGridData().length === 0) {
+    //     notify('Por favor, adicione ao menos um endereço antes de prosseguir', 'error', 3000);
+    //     return;
+    //   }
+    //
+    // }
+
+    formatPhone(e: ChangeEvent) {
+        let phone = e.component.instance().option('value');
+        this.cliente.telefone = FormatAndMaks.formatPhoneNumber(phone!);
+    }
+
+    override getMainService(): any {
+        return this.mainService;
+    }
 }
--- XamaLanche-Angular/src/app/shared/crud/abstract.crud.ts
+++ XamaLanche-Angular/src/app/shared/crud/abstract.crud.ts
@@ -1,117 +1,130 @@
-import {Directive, Injector, Input, ViewChild} from "@angular/core";
-import {CrudComponent} from "../components/base-crud/crud.component";
+import {Directive, Injector, Input} from "@angular/core";
 import {Observable} from "rxjs";
 import {AbstractCrudService} from "../services/abstract.crud.service";
 import {HttpClient} from "@angular/common/http";
 import {BaseComponent} from "./base.crud";
+import notify from "devextreme/ui/notify";
 
 @Directive()
 export abstract class AbstractCrud<T,F> extends BaseComponent{
 
-  @Input()
-  filter:F;
+    @Input()
+    filter:F;
 
-  @Input()
-  model:T;
+    @Input()
+    model:T;
 
-  dataSource: T[] = [];
+    dataSource: T[] = [];
 
-  http: HttpClient
+    http: HttpClient
 
-  getFilter(): any {
-    return this.filter;
-  }
+    getFilter(): any {
+        return this.filter;
+    }
 
-  setFilter(f: F) {
-    this.filter = f;
-  }
+    setFilter(f: F) {
+        this.filter = f;
+    }
 
-  getModel(): any {
-    return this.model;
-  }
+    getModel(): any {
+        return this.model;
+    }
 
-  setModel(m: T) {
-    this.model = m;
-  }
+    setModel(m: T) {
+        this.model = m;
+    }
 
-  getDataSource(): T[] {
-    return this.dataSource;
-  }
-
-
-  /** ----------- INICIO DE METODOS ----------- */
+    getDataSource(): T[] {
+        return this.dataSource;
+    }
 
 
-  /** Metodo chamado antes da execução do save,
-   * Pode ser usado para alterar algum dado do model antes de invocar o save*/
-
-  beforeDoSave(): Observable<any> | null {
-    return null;
-  }
-
-  /** Metodo chamado depois da execução do save,*/
-  afterDoSave() {
-
-  }
-
-  /** Metodo chamado para o Save */
-  doSave(model:any){
-    this.beforeDoSave();
-    this.getMainService().save(model).subscribe(res =>{
-      return res
-    });
-  }
+    /** ----------- INICIO DE METODOS ----------- */
 
 
+    /** Metodo chamado antes da execução do save,
+     * Pode ser usado para alterar algum dado do model antes de invocar o save*/
+
+    beforeDoSave(): Observable<any> | null {
+        return null;
+    }
+
+    /** Metodo chamado depois da execução do save,*/
+    afterDoSave() {
+
+    }
+
+    /** Metodo chamado para o Save */
+    doSave(model:any) {
+        this.beforeDoSave();
+        if(this.validateSave()){
+            this.getMainService().save(model).subscribe(res => {
+                if (res.ok) {
+                    notify('Salvo com sucesso', 'success', 3000);
+                }
+                return res;
+            },error => {
+                notify('Algo deu errado ao tentar salvar', 'error', 3000);
+                return error;
+
+            })
+        }
+    }
+
+    /** Metodo chamado depois do beforeSave e antes da execução do Save */
+
+    validateSave():Boolean{
+        return true;
+    }
 
 
-  /** Metodo chamado antes da execução do filtro */
+    /** Metodo chamado antes da execução do filtro */
 
-  beforeDoFilter() {
+    beforeDoFilter() {
 
-  }
+    }
 
-  /** Metodo chamado apos a execução do filtro */
-  afterDoFilter() {
+    /** Metodo chamado apos a execução do filtro */
+    afterDoFilter() {
 
-  }
+    }
 
-  /** Metodo de Filtro onde contem o beforeDoFilter antes da execução */
+    /** Metodo de Filtro onde contem o beforeDoFilter antes da execução */
 
-  doFilter(){
-    this.beforeDoFilter();
-    return this.getMainService().getAll().subscribe(res => {
-      if(res.body){
-        this.dataSource = res.body
-      }
-      
-    });
-  }
+    doFilter(){
+        this.beforeDoFilter();
+        return this.getMainService().getAll().subscribe(res => {
+            if(res.body){
+                this.dataSource = res.body
+            }
 
-  /** Metodo do botao Clear */
-  doClear(){
+        });
+    }
 
-  }
+    /** Metodo do botao Clear */
+    doClear(){
 
-
-
-
-  /** ----------- METODO PRINCIPAL ----------- */
-
-  /** Metodo onde recebera o service da classe que extende e
-   * fara sera responsavel por executar os metodos criados acima */
-
-  public getMainService(){
-    return new AbstractCrudService<T,F>(this.http,`/${this.urlPrefix}`);
-  }
+    }
 
 
 
-  /** ----------- Construtor ----------- */
+
+    /** ----------- METODO PRINCIPAL ----------- */
+
+    /** Metodo onde recebera o service da classe que extende e
+     * fara sera responsavel por executar os metodos criados acima */
+
+    public getMainService(){
+        return new AbstractCrudService<T,F>(this.http,`/${this.urlPrefix}`);
+    }
 
 
-  constructor( private _injector:Injector,
-               private urlPrefix:string) {
-    super(_injector)
-  }
+
+    /** ----------- Construtor ----------- */
+
+
+    constructor( private _injector:Injector,
+                 private urlPrefix:string) {
+        super(_injector)
+    }
 }

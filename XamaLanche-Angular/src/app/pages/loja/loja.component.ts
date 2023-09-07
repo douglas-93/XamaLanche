707777d5cb6e193be0ddef268a2dc7c380cae60c
Douglas Andrade de Oliveira <douglasaoliveira93@gmail.com>
2023-09-07 17:19:04 -0300

    Acrescimo do endereço

--- XamaLanche-Angular/src/app/pages/loja/loja.component.ts
+++ XamaLanche-Angular/src/app/pages/loja/loja.component.ts
@@ -1,66 +1,65 @@
-import {Component, Injector, OnInit, ViewChild} from '@angular/core';
+import {Component, Injector, ViewChild} from '@angular/core';
 import {LojaService} from "../../shared/services/loja.service";
 import {Loja} from "../../shared/models/loja";
 import {ModeEnum} from "../../shared/enum/mode.enum";
 import {Router} from "@angular/router";
 import {DxFormComponent} from "devextreme-angular";
-import {Endereco} from "../../shared/models/endereco";
 import {SituacaoAtivoInativoEnum} from "../../shared/enum/situacao.ativo.inativo.enum";
 import {AbstractCrud} from "../../shared/crud/abstract.crud";
 import {Observable} from "rxjs";
+import {EnderecoFormComponent} from "../../shared/components/endereco-form/endereco-form.component";
 
 @Component({
-  selector: 'app-loja',
-  templateUrl: './loja.component.html',
-  styleUrls: ['./loja.component.scss']
+    selector: 'app-loja',
+    templateUrl: './loja.component.html',
+    styleUrls: ['./loja.component.scss']
 })
-export class LojaComponent extends AbstractCrud<Loja,any>  {
+export class LojaComponent extends AbstractCrud<Loja, any> {
 
-  @ViewChild("mainForm",{static:false}) mainForm: DxFormComponent
+    @ViewChild("mainForm", {static: false}) mainForm: DxFormComponent;
 
-  Mode:ModeEnum = ModeEnum.List
+    @ViewChild('formularioEndereco', {static: false})
+    formularioEndereco: EnderecoFormComponent;
 
-
-
-  loja:Loja;
-  protected readonly ModeEnum = ModeEnum;
-  situacaoLojas:string[] = Object.values(SituacaoAtivoInativoEnum);
-  nomeLojaFilter: string;
-  endereco:Endereco[]=[]
-
-  constructor(private injector:Injector,
-              private mainService:LojaService,
-              private router: Router) {
-    super(injector,"/loja");
-
-  }
+    Mode: ModeEnum = ModeEnum.List
 
 
-  protected override doOnInit() {
-      if(this.model == null){
-        this.model = new Loja()
-      }
-    super.doOnInit();
-  }
+    loja: Loja;
+    situacaoLojas: string[] = Object.values(SituacaoAtivoInativoEnum);
+    nomeLojaFilter: string;
+    protected readonly ModeEnum = ModeEnum;
 
+    constructor(private injector: Injector,
+                private mainService: LojaService,
+                private router: Router) {
+        super(injector, "/loja");
 
-  override doClear(){
-    this.mainForm.instance.resetValues()
-    // this.nomeLojaFilter = ""
-  }
+    }
 
-  novaLoja() {
-    this.router.navigate(['pages', 'loja'])
-    this.Mode = ModeEnum.Edit
-  }
+    override doClear() {
+        this.mainForm.instance.resetValues()
+    }
 
+    novaLoja() {
+        this.router.navigate(['pages', 'loja'])
+        this.Mode = ModeEnum.Edit
+    }
 
-  override beforeDoSave(): Observable<any> | null {
-    console.log(this.model)
-    return super.beforeDoSave();
-  }
+    override beforeDoSave(): Observable<any> | null {
+        if (this.formularioEndereco.getGridData().length > 0) {
+            this.model.endereco = this.formularioEndereco.getGridData();
+        }
+        return super.beforeDoSave();
+    }
 
-  override getMainService(): any {
-    return this.mainService;
-  }
+    override getMainService(): any {
+        return this.mainService;
+    }
+
+    protected override doOnInit() {
+        if (this.model == null) {
+            this.model = new Loja()
+        }
+        super.doOnInit();
+    }
 }
